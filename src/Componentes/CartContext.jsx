468f1638ffe56d11363d0e360899a7bc6e5d1c25
Nicolas Caretta <nicocaretta_20@hotmail.com>
2022-04-26 10:49:54 -0300

    desafio cart context

--- src/Componentes/CartContext.jsx
+++ src/Componentes/CartContext.jsx
@@ -1,36 +1,38 @@
-import React, { createContext, useContext } from "react";
+import React, { useContext, createContext } from "react";
 import { useState } from "react";
 
-export const CartContext = createContext({});
-export const useCartContext = () => useContext(CartContext);
+export const CartContext = createContext({})
+export const useCartContext = () => useContext(CartContext)
 
-function CartProvider (props) {
-    const [cart, setCart] = useState([]);
+function CartProvider(props) {
+    const [cart, setCart] = useState([])
 
-    const inCart = (id) => { cart.some(item => item.id === id) }
+    const isInCart = (id) => { cart.some(item => item.id === id) }
 
-    const clearCart = () => {
-        setCart([]);
-        const removeItem = (id) => setCart(cart.filter(item => item.id !== id));
-        const addToCart = (item, quantity) => {
-            if (inCart(item.id)) {
-                const newCart = cart.map(cartElement => {
-                    if (cartElement.id === item.id) {
-                        return { ...cartElement, quantity: cartElement.quantity + quantity }
-                    } else return cartElement
-                })
-                setCart(newCart)
-            } else {
-                setCart(prev => [...prev, { ...item, quantity }])
-            }
+    const clearCart = () => { setCart([]) }
+
+    const removeItem = (id) => setCart(cart.filter(item => item.id !== id))
+
+    const addToCart = (item, quantity) => {
+        if (isInCart(item.id)) {
+            const newCart = cart.map(cartElement => {
+                if (cartElement.id === item.id) {
+                    return { ...cartElement, quantity: cartElement.quantity + quantity }
+                } else return cartElement
+            })
+            setCart(newCart)
+        } else {
+            setCart(prev => [...prev, { ...item, quantity }])
         }
-        return (
-            <>
-                <CartContext.Provider value={{ cart, setCart, addToCart, clearCart, removeItem }}>
-                    {props.children}
-                </CartContext.Provider>
-            </>
-        );
-    };
+    }
 
-    export default CartProvider;
+    return (
+        <>
+            <CartContext.Provider value={{ cart, setCart, addToCart, clearCart, removeItem }}>
+                {props.children}
+            </CartContext.Provider>
+        </>
+    )
+}
+
+export default CartProvider;
--- src/Componentes/Item/ItemDetail.jsx
+++ src/Componentes/Item/ItemDetail.jsx
@@ -1,20 +1,15 @@
-import React, {useState, useContext} from 'react';
+import React from 'react';
 import Card from "react-bootstrap/Card"
-import { Link } from 'react-router-dom';
 import ItemCount from './ItemCount';
 import { useCartContext } from '../CartContext';
 
 function ItemDetail({ id, nombre, precio, imagen }) {
-    const [count, setCount] = useState(0);
     const { addToCart } = useCartContext()
 
     function handleOnAdd(q) {
         addToCart({ id, nombre, precio, imagen }, q)
     }
 
-    const number = (count === 0)
-
-    
     return (
         <Card key={id} style={{ width: '18rem' }}>
             <Card.Img variant="top" src={imagen} />
@@ -23,7 +18,7 @@ function ItemDetail({ id, nombre, precio, imagen }) {
                 <Card.Text>
                     $ {precio}
                 </Card.Text>
-                {number ? <ItemCount stock={5} onAdd={handleOnAdd}></ItemCount> : <Link to={"/Cart"}>Ir al carrito</Link>}
+                <ItemCount stock={5} onAdd={handleOnAdd}></ItemCount>
             </Card.Body>
         </Card>
     )
